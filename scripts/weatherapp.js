d219a3243c8ad98288c574a3d9660f9e2ceafb0a
clintonman <ireese@clintons3d.com>
2015-10-27 07:15:10 -0700

    cleaner autocomplete search
    cleaner autocomplete search via array

--- scripts/weatherapp.js
+++ scripts/weatherapp.js
@@ -3,35 +3,30 @@ var myApp = angular.module('weatherApp', []);
 myApp.controller("weatherCtrl", ["$scope", "$http", "getCityList", function ($scope, $http, getCityList) {
     this.section = "first section";
     this.searchresult1 = [];
+    this.searches = [{"search":"",
+                      "searchresult":[]
+                     },
+                    {"search":"",
+                     "searchresult":[]
+                    }];
 
     self = this;
 
-    this.updatesearch = function (list) {
-        if(list==1) {
-            if (this.search1 && this.search1.length > 2) {
-                getCityList.getem(this.search1, list);
+    this.updatesearch = function (index) {
+        var searchfield = this.searches[index].search;
 
-            }
-            else {
-                self.searchresult1 = [];
-            }
+        if(searchfield && searchfield.length > 2) {
+            getCityList.getem(searchfield, index);
         }
-        if(list==2) {
-            if (this.search2 && this.search2.length > 2) {
-                getCityList.getem(this.search2, list);
-
-            }
-            else {
-                self.searchresult2 = [];
-            }
+        else {
+            this.searches[index].searchresult = [];
         }
     };
 
 }]);
 
 myApp.service("getCityList", ["$http", function($http, searchfield){
-    self=this;
-    this.getem = function(searchfield, list) {
+    this.getem = function(searchfield, index) {
         
         var myurl = "http://autocomplete.wunderground.com/aq?cb=JSON_CALLBACK&query=" + searchfield ;
 
@@ -39,24 +34,15 @@ myApp.service("getCityList", ["$http", function($http, searchfield){
                 "method": "JSONP",
                 "url": myurl
             })
-            if(list==1) {
-                return myhttp.then(function(response) {
+
+        return myhttp.then(function(response) {
                     if(response.status == 200) {
-                       self.searchresult1 = response.data.RESULTS;
+                       self.searches[index].searchresult = response.data.RESULTS;
                     }
-                }, function (response) {
-                     self.searchresult1 = ["some error"];
+                },
+               function (response) {
+                    self.searches[index].searchresult = ["some error"];
                 });
-            }
-            if(list==2) {
-                return myhttp.then(function(response) {
-                    if(response.status == 200) {
-                       self.searchresult2 = response.data.RESULTS;
-                    }
-                }, function (response) {
-                     self.searchresult2 = ["some error"];
-                });
-            }
         
     };
     
