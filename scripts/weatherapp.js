0ad7a00a98c06a12722d468f4f573e4d058e4c99
clintonman <ireese@clintons3d.com>
2015-10-26 18:34:40 -0700

    autocomplete api on both search fields
    autocomplete api code is not pretty but it works, next will clean it up

--- scripts/weatherapp.js
+++ scripts/weatherapp.js
@@ -1,33 +1,64 @@
 var myApp = angular.module('weatherApp', []);
 
-myApp.controller("weatherCtrl", ["$scope", "$http", "$filter", function ($scope, $http, $filter) {
+myApp.controller("weatherCtrl", ["$scope", "$http", "getCityList", function ($scope, $http, getCityList) {
     this.section = "first section";
-    this.searchresult1 = "waiting...";
-    self = this;
-    
-    this.updatesearch = function () {
-        console.log(this.search1);
-        //http://autocomplete.wunderground.com/aq?query=Trabuco
-        if (this.search1 && this.search1.length > 3) {
-            var myurl = "http://autocomplete.wunderground.com/aq?query=" + this.search1 + "&cb=JSON_CALLBACK";
-            console.log(myurl);
+    this.searchresult1 = [];
 
-            $http({
-                    "method": "JSONP",
-                    "url": myurl
-                })
-                .then(function(response) {
-                    if(response.status == 200) {
-                        console.log(response.data);
-                        self.searchresult1 = $filter('json')(response.data);
-                    } else {
-                        console.log("not yet");
-                    }
-                }, function (response) {
-                    //console.log(response);
-                    self.searchresult1 = "some error";
-                });
+    self = this;
+
+    this.updatesearch = function (list) {
+        if(list==1) {
+            if (this.search1 && this.search1.length > 2) {
+                getCityList.getem(this.search1, list);
+
+            }
+            else {
+                self.searchresult1 = [];
+            }
+        }
+        if(list==2) {
+            if (this.search2 && this.search2.length > 2) {
+                getCityList.getem(this.search2, list);
+
+            }
+            else {
+                self.searchresult2 = [];
+            }
         }
     };
 
-     }]);
+}]);
+
+myApp.service("getCityList", ["$http", function($http, searchfield){
+    self=this;
+    this.getem = function(searchfield, list) {
+        
+        var myurl = "http://autocomplete.wunderground.com/aq?cb=JSON_CALLBACK&query=" + searchfield ;
+
+        var myhttp = $http({
+                "method": "JSONP",
+                "url": myurl
+            })
+            if(list==1) {
+                return myhttp.then(function(response) {
+                    if(response.status == 200) {
+                       self.searchresult1 = response.data.RESULTS;
+                    }
+                }, function (response) {
+                     self.searchresult1 = ["some error"];
+                });
+            }
+            if(list==2) {
+                return myhttp.then(function(response) {
+                    if(response.status == 200) {
+                       self.searchresult2 = response.data.RESULTS;
+                    }
+                }, function (response) {
+                     self.searchresult2 = ["some error"];
+                });
+            }
+        
+    };
+    
+    
+}]);
